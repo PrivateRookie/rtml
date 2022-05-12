906c52f6503eafa6e44ed1763c862971b217ecf0
PrivateRookie <996514515@qq.com>
2022-05-12 09:22:52 +0800

    feat: allow custom event type

--- crates/lab/src/lib.rs
+++ crates/lab/src/lib.rs
@@ -3,18 +3,23 @@ use wasm_bindgen::{prelude::*, JsCast};
 use web_sys::{Document, Element};
 
 pub trait Template {
-    fn take(
+    /// provide tag name, content, attr, styles and listeners
+    /// to create html element
+    fn resources(
         &self,
     ) -> (
         &'static str,
         &Content,
-        HashMap<EventKind, Box<dyn FnOnce() -> Box<dyn FnMut()> + '_>>,
+        HashMap<&str, Box<dyn FnOnce() -> Box<dyn FnMut()> + '_>>,
     );
 
+    /// after calling dom api to create element,
+    /// replace marker ele field with real element
     fn set_element(&self, element: Element);
 
+    /// generate html element and add event bindings
     fn render(&self, parent: &Element, doc: &Document) -> Result<Element, JsValue> {
-        let (name, content, listeners) = self.take();
+        let (name, content, listeners) = self.resources();
         let ele = match content {
             Content::Null => parent.clone(),
             Content::Text(text) => {
@@ -57,28 +62,96 @@ pub trait Template {
 
 pub type TemplateList = Vec<Box<dyn Template>>;
 
+/// html element content
 pub enum Content {
+    /// empty tag
     Null,
+    /// this element has children
     List(TemplateList),
+    // TODO add html escape checking
+    /// normal text content
     Text(String),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum EventKind {
-    Click,
-    Abort,
+pub struct EventKind(&'static str);
+
+#[allow(non_upper_case_globals)]
+impl EventKind {
+    pub const Abort: &'static str = "abort";
+    pub const Autocomplete: &'static str = "autocomplete";
+    pub const AutocompleteError: &'static str = "autocompleteerror";
+    pub const Blur: &'static str = "blur";
+    pub const Cancel: &'static str = "cancel";
+    pub const CanPlay: &'static str = "canplay";
+    pub const CanPlayThrough: &'static str = "canplaythrough";
+    pub const Change: &'static str = "change";
+    pub const Click: &'static str = "click";
+    pub const Close: &'static str = "close";
+    pub const ContextMenu: &'static str = "contextmenu";
+    pub const CueChange: &'static str = "cuechange";
+    pub const DblClick: &'static str = "dblclick";
+    pub const Drag: &'static str = "drag";
+    pub const DragEnd: &'static str = "dragend";
+    pub const DragEnter: &'static str = "dragenter";
+    pub const DragExit: &'static str = "dragexit";
+    pub const DragLeave: &'static str = "dragleave";
+    pub const DragOver: &'static str = "dragover";
+    pub const DragStart: &'static str = "dragstart";
+    pub const Drop: &'static str = "drop";
+    pub const DurationChange: &'static str = "durationchange";
+    pub const Emptied: &'static str = "emptied";
+    pub const Ended: &'static str = "ended";
+    pub const Error: &'static str = "error";
+    pub const Focus: &'static str = "focus";
+    pub const Input: &'static str = "input";
+    pub const Invalid: &'static str = "invalid";
+    pub const KeyDown: &'static str = "keydown";
+    pub const Keypress: &'static str = "keypress";
+    pub const KeyUp: &'static str = "keyup";
+    pub const Load: &'static str = "load";
+    pub const LoadedData: &'static str = "loadeddata";
+    pub const LoadedMetadata: &'static str = "loadedmetadata";
+    pub const LoadStart: &'static str = "loadstart";
+    pub const MouseDown: &'static str = "mousedown";
+    pub const MouseEnter: &'static str = "mouseenter";
+    pub const MouseLeave: &'static str = "mouseleave";
+    pub const MouseMove: &'static str = "mousemove";
+    pub const MouseOut: &'static str = "mouseout";
+    pub const Mouseover: &'static str = "mouseover";
+    pub const MouseUp: &'static str = "mouseup";
+    pub const MouseWheel: &'static str = "mousewheel";
+    pub const Pause: &'static str = "pause";
+    pub const Play: &'static str = "play";
+    pub const Playing: &'static str = "playing";
+    pub const Progress: &'static str = "progress";
+    pub const RateChange: &'static str = "ratechange";
+    pub const Reset: &'static str = "reset";
+    pub const Resize: &'static str = "resize";
+    pub const Scroll: &'static str = "scroll";
+    pub const Seeked: &'static str = "seeked";
+    pub const Seeking: &'static str = "seeking";
+    pub const Select: &'static str = "select";
+    pub const Show: &'static str = "show";
+    pub const Sort: &'static str = "sort";
+    pub const Stalled: &'static str = "stalled";
+    pub const Submit: &'static str = "submit";
+    pub const Suspend: &'static str = "suspend";
+    pub const TimeUpdate: &'static str = "timeupdate";
+    pub const Toggle: &'static str = "toggle";
+    pub const VolumeChange: &'static str = "volumechange";
+    pub const Waiting: &'static str = "waiting";
+
+    pub fn new(kind: &'static str) -> Self {
+        Self(kind)
+    }
 }
 
 impl AsRef<str> for EventKind {
     fn as_ref(&self) -> &str {
-        match self {
-            EventKind::Click => "click",
-            EventKind::Abort => "abort",
-        }
+        self.0
     }
 }
-
-pub type Listener = Box<dyn Fn() -> Box<dyn FnMut()>>;
 
 pub trait Markers {
     fn set_this(&self, element: Element);
@@ -88,24 +161,23 @@ pub struct Unit<M: Markers> {
     pub name: &'static str,
     pub content: Content,
     pub markers: M,
-    pub listeners: HashMap<EventKind, Box<dyn Fn(M) -> Box<dyn FnMut()>>>,
+    pub listeners: HashMap<&'static str, Box<dyn Fn(M) -> Box<dyn FnMut()>>>,
 }
 
 impl<M: Markers + Clone> Template for Unit<M> {
-    fn take(
+    fn resources(
         &self,
     ) -> (
         &'static str,
         &Content,
-        HashMap<EventKind, Box<dyn FnOnce() -> Box<dyn FnMut()> + '_>>,
+        HashMap<&str, Box<dyn FnOnce() -> Box<dyn FnMut()> + '_>>,
     ) {
-        let factories: HashMap<EventKind, _> = self
+        let factories: HashMap<&'static str, _> = self
             .listeners
             .iter()
             .map(|(kind, func)| {
-                let cb = Box::new(move || {
-                    func(self.markers.clone())
-                }) as Box<dyn FnOnce() -> Box<dyn FnMut()>>;
+                let cb = Box::new(move || func(self.markers.clone()))
+                    as Box<dyn FnOnce() -> Box<dyn FnMut()>>;
                 let kind = kind.clone();
                 (kind, cb)
             })
@@ -160,8 +232,12 @@ impl<M: Markers + Clone> Unit<M> {
         self.markers.clone()
     }
 
-    pub fn on(mut self, kind: EventKind, listener: Box<dyn Fn(M) -> Box<dyn FnMut()>>) -> Self {
-        self.listeners.insert(kind, listener);
+    pub fn on<K: Into<&'static str>>(
+        mut self,
+        kind: K,
+        listener: Box<dyn Fn(M) -> Box<dyn FnMut()>>,
+    ) -> Self {
+        self.listeners.insert(kind.into(), listener);
         self
     }
 }
